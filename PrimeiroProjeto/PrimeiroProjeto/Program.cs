8579db1f4c22bc88095577a3eb69e73d42cfd528
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-04-28 14:30:27 -0300

    Implementar o aplicativo de console básico em `Program.cs` com mensagem de boas-vindas.

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿// Screen Sound
+
+string mensagemDeBoaVinda = "Bem-vindo ao meu primeiro projeto em C#!";
+Console.WriteLine(mensagemDeBoaVinda);
+
+string curso = "Curso de C#";
+string aluno = "Paulo Passos";
+
+Console.WriteLine("Curso: " + curso + " Aluno: " +aluno);
