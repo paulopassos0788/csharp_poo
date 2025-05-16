3857da0acdfe8e322a1080991a8dfe775a4731a1
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-05-16 08:22:44 -0300

    integrando uma api api externa
    - Exploramos a importância de realizarmos requisições utilizando a classe HttpClient do C# para interagirmos com APIs. Aprendemos como criar uma requisição e receber a resposta correspondente do servidor;
    
    - Destacamos a necessidade de tratarmos os erros ao realizarmos a requisição, utilizando try-catch para capturar as exceções que possam ocorrer durante o processo. Isso nos permite lidar com situações inesperadas de forma adequada e fornecer uma experiência mais robusta aos usuários de nossa aplicação.

---
+++ ScreenSound04/ScreenSound04/Program.cs
@@ -0,0 +1,12 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    } catch (Exception ex)
+    {
+        Console.WriteLine($"Erro ao acessar a API. {ex.Message}");
+    }
+
+}
