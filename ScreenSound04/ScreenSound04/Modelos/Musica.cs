cecafbc83c1104ecec90a2f23c04948456aab8c0
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-05-16 10:34:51 -0300

    desserialização
    - Criamos uma classe com propriedades mapeadas para alguns campos da API com JsonPropertyName e realizamos a desserialização da resposta;
    
    - Desserializamos os dados recebidos em um formato adequado para manipulação em nosso código. A serialização nos permite converter os dados recebidos em um formato como JSON em objetos que podemos trabalhar em nosso programa, facilitando a manipulação e extração das informações relevantes.

---
+++ ScreenSound04/ScreenSound04/Modelos/Musica.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound04.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set;}
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+        
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+        
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+    
+    public void ExibirMusica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao / 1000} ms");
+        Console.WriteLine($"Gênero: {Genero}");
+    }
+}
--- ScreenSound04/ScreenSound04/Program.cs
+++ ScreenSound04/ScreenSound04/Program.cs
@@ -1,9 +1,13 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using ScreenSound04.Modelos;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        musicas[10].ExibirMusica();
     } catch (Exception ex)
     {
         Console.WriteLine($"Erro ao acessar a API. {ex.Message}");
